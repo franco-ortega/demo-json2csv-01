34cfcaf1092aa52b61759b1bdad4727b3ab54117
Franco Ortega <francoortega@Francos-MacBook-Pro.local>
2021-12-22 18:26:21 -0800

    fix typos

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { Parser } = require('json2csv');
 const catJsonData = require('./data/cats.json');
 
 // This file contains three implementations of creating CSV files
-// The first implentation creates a CSV file with no modifications to the data
-// The second implentation creates a CSV file that only includes select columns of data
-// The third implentation creates a CSV file that creates custom titles for the columns and provides default values for missing/empty data 
+// The first implementation creates a CSV file with no modifications to the data
+// The second implementation creates a CSV file that only includes select columns of data
+// The third implementation creates a CSV file that creates custom titles for the columns and provides default values for missing/empty data 
     // (see index2.js for the third implementation)
 
 
@@ -30,7 +30,7 @@ fs.writeFile("./csvFiles/indexCatsFromJson.csv", csvData, "utf8", function (err)
 
 
 // ****** SECOND IMPLEMENTATION ****** //
-// This implemenation utilizes the fields property (an array of strings) to select the specific properties that will be included (as columns) in the file; all other properties will be omitted
+// This implementation utilizes the fields property (an array of strings) to select the specific properties that will be included (as columns) in the file; all other properties will be omitted
 
 // This will only include the name and age properties
 const fields = ['name', 'age'];
